9cf816e5284ec831e1000f307cbaae3d29be6916
AbidFAli <ali.655@osu.edu>
2021-11-09 15:27:02 -0500

    Fixed eslint errors by adding babel parser for eslint

--- src/utils/errors.js
+++ src/utils/errors.js
@@ -1,23 +1,21 @@
-
-class UserCreationError extends Error{
-  constructor(message){
+class UserCreationError extends Error {
+  constructor(message) {
     super(message);
-    this.name = UserCreationError.name
+    this.name = UserCreationError.name;
   }
   static name = "UserCreationError";
   static MESSAGE_NONUNIQUE_USERNAME = "Username must be unique";
 }
 
-class SearchError extends Error{
-  constructor(message){
-    super(message)
-    this.name = SearchError.name
+class SearchError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = SearchError.name;
   }
-  static name = "SearchError"
-
+  static name = "SearchError";
 }
 
 module.exports = {
   UserCreationError,
-  SearchError
-}
+  SearchError,
+};
